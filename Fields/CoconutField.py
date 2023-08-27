5c1172dfbde45604c16c6f1b3bf404529d5819c2
rudynakodach <whoppe7292@gmail.com>
2023-08-27 10:52:21 +0200

    optimized coconut field paths

--- Fields/CoconutField.py
+++ Fields/CoconutField.py
@@ -3,13 +3,36 @@ from Utils import Utils
 from . import PepperPatch
 
 def goto(start: bool = True):
-    PepperPatch.goto(False)
-
-    Utils.press("d", 5)
-    Utils.press("s", 7.5)
-    Utils.press("space", .1)
-    Utils.press("s", 9)
+    Utils.press("w", 5)
+    Utils.rotateCamera(2)
+    Utils.press("w", 7.5)
+    Utils.press("space", .25)
+    Utils.press("w", .1)
+    Utils.press("w", 7.5)
+    Utils.press("a", 2)
+    Utils.press("s", 0.05)
+    Utils.press("space", 0.08)
     Utils.press("w", .5)
+
+    Utils.getPdi().keyDown("a")
+    Utils.getPdi().keyDown("w")
+    sleep(1)
+    Utils.getPdi().keyUp("a")
+    Utils.getPdi().keyUp("w")
+    Utils.press("space")
+    
+    Utils.getPdi().keyDown("a")
+    Utils.getPdi().keyDown("space")
+    sleep(4)
+    Utils.getPdi().keyUp("a")
+    Utils.getPdi().keyUp("space")
+    
+    Utils.press("w", .1)
+
+    Utils.press("d", 3)
+    Utils.press("a", 3)
+    Utils.press("s", 7.5)
+    Utils.press("w", .4)
 
     if start:
         Utils.placeSprinklersDirectional("w", .2, True)
@@ -21,18 +44,10 @@ def goto(start: bool = True):
 def getBack(loop = False):
     Utils.press("w", 7.5)
     Utils.press("d", 5)
-    Utils.press("a", .175)
-    Utils.press("space", .1)
-    Utils.press("w", 7.5)
-    Utils.press("a", 3)
     Utils.press("s", 7.5)
-    Utils.press("d", 7.5)
-    Utils.press("s", 9)
+    Utils.press("d", 5)
     Utils.press("a", 5)
     Utils.rotateCamera(-2)
-    Utils.press("s", .2)
-    Utils.press("d", 7.5)
-    Utils.press("w", .5)
     Utils.press("s", .35)
 
     for _ in range(60):
